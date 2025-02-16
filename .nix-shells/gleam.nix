ce885f2a82e1e446d440e8db687ce29e1fcfe925
Tressa Danvers <TDanvers@protonmail.ch>
2025-02-16 04:39:08 -0500

    [gleam/hello-world] final submission

---
+++ .nix-shells/gleam.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+let
+in mkShell {
+  buildInputs = [
+    erlang
+    gleam
+    rebar3
+  ];
+
+  shellHook = ''
+    . "$HOME/.bashrc"
+
+    esc=$(printf "\e")
+    Fred="$esc[31m"
+    f="$esc[39m"
+    B="$esc[01m"
+    b="$esc[22m"
+
+    PS1=$B'[ '$Fred'nix-shell'$f' ]-'$b"$PS1"
+
+    unset esc Fred f B b
+  '';
+}
