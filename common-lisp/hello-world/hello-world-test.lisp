caf6e5a33bfd9cbee2a1cc5eb569f49303f9ad93
Tressa Danvers <TDanvers@protonmail.ch>
2025-02-19 19:15:13 -0500

    condensed [common-lisp/hello-world] tests

--- common-lisp/hello-world/hello-world-test.lisp
+++ common-lisp/hello-world/hello-world-test.lisp
@@ -1,18 +1,13 @@
-;; Ensures that hello-world.lisp and the testing library are always loaded
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "hello-world")
   (quicklisp-client:quickload :fiveam))
 
-;; Defines the testing package with symbols from hello-world and FiveAM in scope
-;; The `run-tests` function is exported for use by both the user and test-runner
 (defpackage :hello-world-test
   (:use :cl :fiveam)
   (:export :run-tests))
 
-;; Enter the testing package
 (in-package :hello-world-test)
 
-;; Define and enter a new FiveAM test-suite
 (def-suite* hello-world-suite)
 
 (test say-hi! (is (equal "Hello, World!" (hello-world:hello))))
