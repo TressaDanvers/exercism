09bc6ca274377f6ede2c894b443b060ed0308e0c
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-09 02:23:19 -0400

    initial dev flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ exercism ];
+        shellHook = ''
+          export PS1="\[\033[1;32m\](exercism)\[\033[0m\] $PS1"
+        '';
+      };
+    });
+}
