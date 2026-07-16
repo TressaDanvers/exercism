76017b7d6e887908cef421a6a58cff9fa580c3b7
Tressa Danvers <TDanvers@protonmail.ch>
2026-07-16 12:35:48 -0400

    [wren/hello-world] init and final

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    wren-console.url = "github:TressaDanvers/wren-console";
   };
   
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, wren-console, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      wrenc = wren-console.packages.${system}.default;
     in {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ exercism ];
+        buildInputs = with pkgs; [ exercism wrenc ];
         shellHook = ''
           export PS1="\[\033[1;32m\](exercism)\[\033[0m\] $PS1"
         '';
